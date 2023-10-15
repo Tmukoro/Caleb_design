eb2784c31f95f56c45818209a94590d4f83799b0
Tmukoro <107497702+Tmukoro@users.noreply.github.com>
2023-10-15 18:23:50 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
 const span = document.querySelector(".span");
 
-const span2 = document.querySelector(".span2")
-
-const span3 = document.querySelector(".span3")
-
-const span4 = document.querySelector(".span4")
-
-const span5 = document.querySelector(".span5")
-
 const subp = document.querySelector(".subp");
-
-const subp2 = document.querySelector(".subp2")
-
-const subp3 = document.querySelector(".subp3")
-
-const subp4 = document.querySelector(".subp4")
-
-const subp5 = document.querySelector(".subp5")
-
 
 span.addEventListener("click", () => {
     span.classList.toggle("active")
@@ -26,11 +9,24 @@ span.addEventListener("click", () => {
 })
 
 
+
+const span2 = document.querySelector(".span2")
+
+const subp2 = document.querySelector(".subp2")
+
+
 span2.addEventListener("click", () => {
     span2.classList.toggle("active")
 
     subp2.classList.toggle("active")
 })
+
+
+
+
+const span3 = document.querySelector(".span3")
+
+const subp3 = document.querySelector(".subp3")
 
 span3.addEventListener("click", () => {
     span3.classList.toggle("active")
@@ -38,14 +34,54 @@ span3.addEventListener("click", () => {
     subp3.classList.toggle("active")
 })
 
+
+
+const span4 = document.querySelector(".span4")
+
+const subp4 = document.querySelector(".subp4")
+
+
 span4.addEventListener("click", () => {
     span4.classList.toggle("active")
 
     subp4.classList.toggle("active")
 })
 
+
+
+const span5 = document.querySelector(".span5")
+
+const subp5 = document.querySelector(".subp5")
+
+
 span5.addEventListener("click", () => {
     span5.classList.toggle("active")
 
     subp5.classList.toggle("active")
 })
+
+
+
+const harmburger = document.querySelector(".harmburger")
+
+const navlinks = document.querySelector(".nav_links")
+
+const apb = document.querySelector(".apb")
+
+const apb2 = document.querySelector(".apb2")
+
+harmburger.addEventListener("click", () => {
+    harmburger.classList.toggle("active")
+
+    navlinks.classList.toggle("active")
+
+    apb.classList.toggle("active")
+
+    apb2.classList.toggle("active")
+})
+
+
+
+
+
+
